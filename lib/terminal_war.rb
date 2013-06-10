aaaf9df1fbc378d7f060f9d43c0e90642805ba98
Michael Dorrance <mdorrance@sbcglobal.net>
2013-06-09 19:22:17 -0500

    changes face suits to value suits, adds additional battles

--- lib/terminal_war.rb
+++ lib/terminal_war.rb
@@ -11,10 +11,10 @@
 
 puts "Welcome to War!"
 
-deck_of_cards = [ "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
-                  "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
-                  "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
-                  "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"]
+deck_of_cards = [ "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "11C", "12C", "13C", "14C",
+                  "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "11D", "12D", "13D", "14D",
+                  "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "11H", "12H", "13H", "14H",
+                  "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "11S", "12S", "13S", "14S"]
 deck_of_cards = deck_of_cards.shuffle
 puts  deck_of_cards.inspect
 
@@ -38,7 +38,7 @@ response = gets.chomp
 
   end
 
-puts "Ready for me to deal the cards?"
+puts "Ready for me to deal the cards? 'Y' or 'N'"
   response = gets.chomp
   if response == "Y" || response == "y"
     puts []
@@ -46,19 +46,36 @@ puts "Ready for me to deal the cards?"
     puts "Too bad, I'm dealing anyways!"
   end
 puts "Dealer has #{player1.length} cards and you have #{player2.length} cards"
-puts "Ready for the first battle? 'Y' or 'N'"
+puts "Ready for battle? 'Y' or 'N'"
   response = gets.chomp
   if response == "Y" || response == "y"
-    puts player1.slice!(0) #shows each players card on the table
-    puts player2.slice!(0) #takes top card out of player array
+    puts "Dealer draws #{player1.slice!(0)}" #shows each players card on the table
+    puts "You draw #{player2.slice!(0)}" #takes top card out of player array
     if player1.slice(0) > player2.slice(0)
       player1 << player1.slice(0)
       player1 << player2.slice(0)
     else  player1.slice(0) < player2.slice(0)
       player2 << player1.slice(0)
       player2 << player2.slice(0)
+
     #after the cards are shown- need to determine value and then push both cards to winner's array
     end
+   puts "Dealer has #{player1.length} cards and you have #{player2.length} cards"
+   puts "Battle again? 'Y' or 'N'?"
+    resonse = gets.chomp
+    if response == "Y" || response == "y"
+    puts "Dealer draws #{player1.slice!(0)}" #shows each players card on the table
+    puts "You draw #{player2.slice!(0)}" #takes top card out of player array
+    if player1.slice(0) > player2.slice(0)
+      player1 << player1.slice(0)
+      player1 << player2.slice(0)
+    else  player1.slice(0) < player2.slice(0)
+      player2 << player1.slice(0)
+      player2 << player2.slice(0)
+
+    #after the cards are shown- need to determine value and then push both cards to winner's array
+    end
+  end
    puts "Dealer has #{player1.length} cards and you have #{player2.length} cards"
   else
     abort("See yeah!")
